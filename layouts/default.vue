5c4606e9a573f85ec264b9b933c32571e32eb54a
oladetounjed <oladetountemitayo@gmail.com>
2021-04-09 11:40:25 +0100

    Resources Imported Globally and Meta Tags Added

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,6 @@
 </template>
 
 <style lang="scss">
-@import url("https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,600;1,700&display=swap");
-@import "~/assets/scss/main.scss";
+
+
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,93 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
-  
+
+  pwa: {
+    manifest: {
+      name: "Unfoto",
+      short_name: "Unfoto",
+      description:
+        "Unfoto is a web app built on Unsplash API dedicated to give users access to lightweight beautiful, free Unsplash images and photos that you can download and use for any project. Better than any royalty free or stock photos. ",
+      background_color: "#ffffff",
+      theme_color: "#084CBF",
+      lang: "en"
+    }
+  },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: "Mini-Unsplash",
+    title: "Unfoto",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Unfoto is a web app built on Unsplash API dedicated to give users access to lightweight beautiful, free Unsplash images and photos that you can download and use for any project. Better than any royalty free or stock photos. "
+      },
+      {
+        name: "theme-color",
+        property: "theme-color",
+        content: "#a5854f"
+      },
+      {
+        name: "apple-mobile-web-app-title",
+        property: "apple-mobile-web-app-title",
+        content: "Unfoto"
+      },
+      {
+        name: "og:title",
+        property: "og:title",
+        content: "Unfoto"
+      },
+      {
+        name: "og:site_name",
+        property: "og:site_name",
+        content: "Unfoto"
+      },
+      {
+        name: "og:image",
+        property: "og:image",
+        content: "/icon.png"
+      },
+      {
+        name: "twitter:title",
+        property: "og:title",
+        content: "Unfoto"
+      },
+      {
+        name: "twitter:site_name",
+        property: "og:site_name",
+        content: "Unfoto"
+      },
+      {
+        name: "twitter:description",
+        property: "twitter:description",
+        content:
+          "Unfoto is a web app built on Unsplash API dedicated to give users access to lightweight beautiful, free Unsplash images and photos that you can download and use for any project. Better than any royalty free or stock photos. "
+      },
+      {
+        name: "twitter:image",
+        content: "/icon.png"
+      }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,600;1,700&display=swap"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      }
+    ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ["@/assets/scss/main.scss"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
